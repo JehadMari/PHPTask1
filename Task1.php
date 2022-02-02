fdd9633cdb12a3cd7b10d187fae37329b5275ea0
JehadMari <96633026+JehadMari@users.noreply.github.com>
2022-02-02 23:10:22 +0200

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = "Hello PHP";
+echo $x;
+
+$y = "<br/><br/>The salary is: ";
+$salary = 1200;
+echo $y . $salary . " JOD";
+
+echo "<h1>Our Services</h1><p>We provide many services in web development and mobile development.</p>";
+?>
+
+</body>
+</html>
